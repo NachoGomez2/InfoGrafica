755702652f6e52b44ed35d2cbf54af776e55b193
Nacho Gomez <nacho.gomez@enti.cat>
2025-02-27 19:38:55 +0100

    Ej hasta el 7

--- ExercicisOpenGL/InformaticaGrafica/main.cpp
+++ ExercicisOpenGL/InformaticaGrafica/main.cpp
@@ -5,7 +5,7 @@ int init(void) {
 
     glClearColor(1.0, 1.0, 1.0, 0.0); // set background color to white
     glMatrixMode(GL_PROJECTION); // set projection parameters
-    gluOrtho2D(0.0, 200.0, 0.0, 150.0); // set 2D orthographic projection
+    gluOrtho2D(-300.0, 250.0, -250.0, 250.0); // set 2D orthographic projection
     return 0;
 }
 
@@ -30,6 +30,7 @@ void Ex2()
     glVertex2f(100.0, 50.0);
     glVertex2f(100.0, 100.0);
     glVertex2f(50.0, 100.0);
+
 }
 
 void Ex3()
@@ -45,6 +46,78 @@ void Ex3()
     glVertex2f(20.0, 80.0);
 }
 
+void Ex4()
+{
+    float matrixReflection[] = {
+        -1.0, 0.0, 0.0, 0.0,
+        0.0, 1.0, 0.0, 0.0,
+        0.0, 0.0, 1.0, 0.0,
+        0.0, 0.0, 0.0, 1.0
+    };
+        
+    glMultMatrixf(matrixReflection);
+    
+    glBegin(GL_TRIANGLES);
+    glVertex2f(40.0, 40.0);
+    glVertex2f(90.0, 100.0);
+    glVertex2f(140.0, 40.0);
+}
+
+void Ex5()
+{
+    float shX = 1.0;
+
+    float matrixShear[] = {
+        1.0, 0.0, 0.0, 0.0, //primera columna
+        shX, 1.0, 0.0, 0.0, //segunda columna
+        0.0, 0.0, 1.0, 0.0, //tecera columna
+        0.0, 1.0, 0.0, 1.0 // cuarta columna
+    };
+    
+    glMultMatrixf(matrixShear);
+
+    glBegin(GL_POLYGON);
+    glVertex2f(50.0, 50.0);
+    glVertex2f(100.0, 50.0);
+    glVertex2f(100.0, 100.0);
+    glVertex2f(50.0, 100.0);
+}
+
+void Ex6()
+{
+    float shY = 0.5;
+
+    float matrixShear[] = {
+        1.0, shY, 0.0, 0.0,
+        0.0, 1.0, 0.0, 0.0,
+        0.0, 0.0, 1.0, 0.0,
+        0.0, 1.0, 0.0, 1.0
+    };
+
+    glMultMatrixf(matrixShear);
+
+    glBegin(GL_POLYGON);
+    glVertex2f(50.0, 50.0);
+    glVertex2f(100.0, 50.0);
+    glVertex2f(100.0, 100.0);
+    glVertex2f(50.0, 100.0);
+}
+
+void Ex7()
+{
+    
+    glTranslatef(20.0, -10.0, 0.0);
+
+    glTranslatef(30.0, 40.0, 0.0);
+    glScalef(2.0, 1.5, 1.0);
+    glTranslatef(-30.0, -40.0, 0.0);
+    
+    glBegin(GL_TRIANGLES);
+    glVertex2f(30.0, 40.0);
+    glVertex2f(80.0, 120.0);
+    glVertex2f(130.0, 40.0);
+}
+
 void display(void) {
 
     glClear(GL_COLOR_BUFFER_BIT); // clear display window
@@ -57,8 +130,11 @@ void display(void) {
     /////////////////EJERCICIOS/////////////////
     //Ex1();
     //Ex2();
-    Ex3();
-
+    //Ex3();
+    //Ex4();
+    //Ex5();
+    Ex6();
+    //Ex7();
 
 
     glEnd(); // end drawing points
